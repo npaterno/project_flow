ccc0526a54da71d4c44ec97ffc56ad957bc19c64
npaterno <nicholas.paterno@gmail.com>
2021-07-30 14:00:53 -0700

    draft

---
+++ demo_files/bad_file_demo.R
@@ -0,0 +1,13 @@
+# Set Working Directory
+
+setwd("C:/Users/nicho/Desktop/demo")
+
+# Load Packages
+
+library(readr)
+
+# Load Data
+
+world_population <- read_csv("world_pop.csv")
+
+View(world_population)
